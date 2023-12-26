4dfe8b766dde252294a6c07f89370a2182ec39bf
abdullahbuhayia <buhayia12@gmail.com>
2023-12-26 21:52:06 +0600

    first commit

---
+++ php/footerbottom.php
@@ -0,0 +1,9 @@
+		
+</div>
+<footer class="container">
+	<h3 style="text-align:center;background:#2A2B3C;margin:0;padding:15px;color:#fff;font-weight:normal">&copy;  Md Abdullah Buhayia <?php ;?> </h3>
+</footer>
+</html>
+
+
+
